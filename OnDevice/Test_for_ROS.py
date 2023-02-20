3b557afec9f886ad82e26c0785b031ac9feacd00
djyanke <92260098+Djyankencsu@users.noreply.github.com>
2023-02-20 16:31:02 -0500

    Made script able to switch between POE and USB mode

--- OnDevice/Test_for_ROS.py
+++ OnDevice/Test_for_ROS.py
@@ -2,10 +2,48 @@ import time
 import depthai as dai
 import numpy as np
 import os
+import argparse
 import pathlib
 import cv2
 import matplotlib.pyplot as plt
 
+#Added an CLI argument to allow switching between USB and POE testing
+parser = argparse.ArgumentParser(
+    prog = "Test for ROS"
+    description= "Provides interface with camera pipeline and generates 3 numpy arrays")
+
+parser.add_argument("-u", "-usb",action="store_true")
+is_usb_cam = parser.parse_args()
+
+def enable_camera(device,shape):
+    depth_out = device.getOutputQueue("depth",1,False)
+    segmentation_queue = device.getOutputQueue("seg out",1,False)
+    rgb_queue = device.getOutputQueue("rgb",1,False)
+    while True:
+        segmentation_labels = segmentation_queue.get()
+        
+        segmentation_labels = np.array(segmentation_labels.getFirstLayerInt32()).reshape(shape[0],shape[1])
+        
+        out = [None,None,None]
+        rgb_data = rgb_queue.get()
+        rgb_img = np.array(rgb_data.getCvFrame())
+        depth_data = depth_out.get()
+        depth_img = np.array(depth_data.getCvFrame())
+        depth_img = (depth_img * (255/depth.initialConfig.getMaxDisparity())).astype(np.uint8)
+        #layers = data_o.getAllLayers()
+        out[0] = rgb_img #shape is (height, width, 3)
+        out[1] = segmentation_labels #shape is (height, width, 1)
+        out[2] = depth_img #shape is default
+
+        #Output for Testing
+        cv2.imshow("RGB",rgb_img)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+	
+        # cv2.imshow("Segmentation",nn_img)
+        # cv2.imshow("Depth",depth_img)
+        # time.sleep(5)
 
 #This section detects the directory that encapsulates the script.
 script_path = str(pathlib.Path(__file__).parent.resolve())
@@ -24,30 +62,30 @@ print("Creating color channel...")
 #instream = pipeline.create(dai.node.XLinkIn)
 #instream.setStreamName("input")
 #instream.out.link(node.input)
-colorCam = pipeline.createColorCamera()
-colorCam.setPreviewSize(nn_shape[0], nn_shape[1])
-colorCam.setInterleaved(False)
-colorCam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-colorCam.setBoardSocket(dai.CameraBoardSocket.RGB)
+RGB_Node = pipeline.createcolorCamera()
+RGB_Node.setPreviewSize(nn_shape[0], nn_shape[1])
+RGB_Node.setInterleaved(False)
+RGB_Node.setResolution(dai.RGB_NodeeraProperties.SensorResolution.THE_1080_P)
+RGB_Node.setBoardSocket(dai.CameraBoardSocket.RGB)
 
-CamOut=pipeline.create(dai.node.XLinkOut)
-CamOut.setStreamName("rgb")
-CamOut.input.setBlocking(False)
-CamOut.input.setQueueSize(1)
-colorCam.preview.link(CamOut.input)
+RGB_Out=pipeline.create(dai.node.XLinkOut)
+RGB_Out.setStreamName("rgb")
+RGB_Out.input.setBlocking(False)
+RGB_Out.input.setQueueSize(1)
+RGB_Node.preview.link(RGB_Out.input)
 print("Done")
 
 print("Creating neural network node...")
-node = pipeline.create(dai.node.NeuralNetwork)
-node.setBlobPath(nn_path)
-node.input.setQueueSize(1)
-node.input.setBlocking(False)
+nn_node = pipeline.create(dai.node.NeuralNetwork)
+nn_node.setBlobPath(nn_path)
+nn_node.input.setQueueSize(1)
+nn_node.input.setBlocking(False)
 
-colorCam.preview.link(node.input)
+RGB_Node.preview.link(nn_node.input)
 
-out = pipeline.create(dai.node.XLinkOut)
-out.setStreamName("output")
-node.out.link(out.input)
+seg_out = pipeline.create(dai.node.XLinkOut)
+seg_out.setStreamName("seg out")
+nn_node.out.link(seg_out.input)
 print("Done")
 
 print("Creating depth channel...")
@@ -55,8 +93,8 @@ monoL = pipeline.create(dai.node.MonoCamera)
 monoR = pipeline.create(dai.node.MonoCamera)
 depth = pipeline.create(dai.node.StereoDepth)
 
-dout = pipeline.create(dai.node.XLinkOut)
-dout.setStreamName("depth")
+depth_out = pipeline.create(dai.node.XLinkOut)
+depth_out.setStreamName("depth")
 monoL.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
 monoL.setBoardSocket(dai.CameraBoardSocket.LEFT)
 
@@ -66,53 +104,16 @@ monoR.setBoardSocket(dai.CameraBoardSocket.RIGHT)
 depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
 monoL.out.link(depth.left)
 monoR.out.link(depth.right)
-depth.depth.link(dout.input)
+depth.depth.link(depth_out.input)
 print("Done")
 
-#Intentionally overwrites the stats file. Copy it if you want to save a certain run's data
 ip = "169.254.54.205"
 cam = dai.DeviceInfo(ip)
-with dai.Device(pipeline,cam) as device:
-    depth_out = device.getOutputQueue("depth",1,False)
-    nn_out = device.getOutputQueue("output",1,False)
-    cam_out = device.getOutputQueue("rgb",1,False)
-    while True:
-        nn_data = nn_out.get()
-        
-        nn_data = np.array(nn_data.getFirstLayerInt32()).reshape(nn_shape[0],nn_shape[1])
-        #nn_data = np.array(nn_data.getCvFrame())
-        soil = np.array([40, 86, 166])
-        clover = np.array([28, 26, 228])
-        broadleaf = np.array([184, 126, 155])
-        grass = np.array([0, 127, 255])
-    
-        nn_img = np.zeros((nn_shape[0],nn_shape[1],3),dtype=np.uint8)
-        nn_img[nn_data==0] = soil
-        nn_img[nn_data==1] = clover
-        nn_img[nn_data==2] = broadleaf
-        nn_img[nn_data==3] = grass
-        
-        out = [None,None,None]
-        cam_data = cam_out.get()
-        cam_img = np.array(cam_data.getCvFrame())
-        depth_data = depth_out.get()
-        depth_img = np.array(depth_data.getCvFrame())
-        depth_img = (depth_img * (255/depth.initialConfig.getMaxDisparity())).astype(np.uint8)
-        #layers = data_o.getAllLayers()
-        out[0] = cam_img
-        out[1] = nn_img
-        out[2] = depth_img
-       
-        print(f"Depth image : {nn_img}")
-        print("------------")
-        #Output for Testing
-        cv2.imshow("RGB",cam_img)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-	
-        # cv2.imshow("Segmentation",nn_img)
-        # cv2.imshow("Depth",depth_img)
-        # time.sleep(5)
+if is_usb_cam:
+    with dai.Device(pipeline) as device:
+        enable_camera(device,nn_shape)
+else:
+    with dai.Device(pipeline,cam) as device:
+        enable_camera(device,nn_shape)
         
         
