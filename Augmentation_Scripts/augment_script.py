864ae01ca9b6e4821f6fc968e559f523440c1637
djyanke <djyanke@ncsu.edu>
2023-04-03 15:49:31 -0400

    Added Label Manipulation
    Script now expects a folder with labels to manipulate in parallel with the training images. Relative paths should also be more reliable.

--- Augmentation_Scripts/augment_script.py
+++ Augmentation_Scripts/augment_script.py
@@ -54,12 +54,18 @@ def ISONoise(image):
     return out
 
 
-
-project_dir = "~/NCSU/PSA/CV_Photo_Augment_Proj/"
-input_dir =  "/Users/Daniel/NCSU/PSA/CV_Photo_Augment_Proj/input/"
-output_dir =  "/Users/Daniel/NCSU/PSA/CV_Photo_Augment_Proj/dataset1/"
-#if not os.path.exists(output_dir):
-#    os.mkdir(output_dir)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+project_dir = os.path.abspath("../../CV_Photo_Augment_Proj/")
+input_dir =  os.path.abspath("../../CV_Photo_Augment_Proj/input/")
+label_dir = os.path.abspath("../../CV_Photo_Augment_Proj/labels/")
+run_name = "dataset2_with_labels"
+output_im_dir =  os.path.abspath("../../CV_Photo_Augment_Proj/"+run_name+"/images/")
+resized_labels = os.path.abspath("../../CV_Photo_Augment_Proj/"+run_name+"/labels/")
+if not os.path.exists(output_im_dir):
+    Path(output_im_dir).mkdir(parents=True)
+if not os.path.exists(resized_labels):
+    Path(resized_labels).mkdir(parents=True)
 
 input_list = os.listdir(input_dir)
 input_list.sort()
@@ -69,20 +75,32 @@ for photo_num in files:
     print("Starting "+photo_num)
     photo = cv2.imread(input_dir+photo_num)
     photo_num = photo_num.split(".")[0]
+    label = cv2.imread(label_dir)
     photo = cv2.cvtColor(photo,cv2.COLOR_BGR2RGB)
     scale = 0.7
-    photo = downscale(photo,scale)
-    dictionary_holder = split(photo,(1024,1024))
+    non_separable = [downscale,split]
+    for i in non_separable:
+        photo = i(photo,scale)
+        label = i(label,scale)
+        scale = (1024,1024) 
+        #Reusing this scale variable for image shape allows the loop to work.
+        #It is just changing to specify the output image shape for the split
+        #function after the scaling. 
+
+    for i in label:
+        #i is dictionary key, which is either "tl", "tr", "bl", or "br" for matching the augmented images. 
+        image = label[i].astype(np.uint8)
+        cv2.imwrite((resized_labels+photo_num+i+".jpg"),cv2.cvtColor(image.astype(np.uint8),cv2.COLOR_RGB2BGR)) 
 
     separable = [blur,PCA,GaussNoise,ISONoise]
 
-    for i in dictionary_holder:
+    for i in photo:
         for j in range(0,10):
             holder = i +"_" #dictionary key is start of character code file name
-            image = dictionary_holder[i].astype(np.uint8)
+            image = photo[i].astype(np.uint8)
             for k in range(0,random.randint(1,7)):
                 [image,char] = separable[random.randint(0,(len(separable)-1))](image)
                 holder = holder + char   
-            cv2.imwrite((output_dir+photo_num+holder+".jpg"),cv2.cvtColor(image.astype(np.uint8),cv2.COLOR_RGB2BGR))
+            cv2.imwrite((output_im_dir+photo_num+holder+".jpg"),cv2.cvtColor(image.astype(np.uint8),cv2.COLOR_RGB2BGR))
 
 
